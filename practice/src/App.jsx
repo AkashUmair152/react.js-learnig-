2ef77d12a567c7b6c3de851becb8d3315ad1736d
Akash Umair <akashumair922@gmail.com>
2024-09-10 11:10:02 +0500

    learning hooks in react.js

---
+++ practice/src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
+        consequuntur ab numquam laborum, quaerat dolorum suscipit quia,
+        voluptatem, itaque repellendus reprehenderit quo nobis. Odio labore illo
+        atque accusamus. Totam ex pariatur earum ab, tempora velit provident
+        veniam illo itaque fugit atque, nobis qui nisi. Tenetur sed perferendis
+        voluptate temporibus possimus asperiores iure doloremque unde, nesciunt
+        dolorum repellendus aliquam id? Blanditiis porro a neque nesciunt hic
+        nemo asperiores aspernatur quia et exercitationem delectus libero
+        quisquam quaerat incidunt facere nostrum quis reiciendis, totam magnam
+        consequatur eos voluptatibus? Corporis, enim quod doloremque atque,
+        dolorum libero ullam praesentium ex exercitationem, fugit autem
+        mollitia! Itaque!
+      </p>
+      <h4>This is Sub heading for my blog</h4>
+    </>
+  );
+};
+
+export default App;
